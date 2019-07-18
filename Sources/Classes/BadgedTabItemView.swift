e68a88635a99e14e84f9980c95deaee70be6fc40
Don Holly <don.holly@gmail.com>
2019-07-17 23:25:40 -0700

    adjust badge padding

--- Sources/Classes/BadgedTabItemView.swift
+++ Sources/Classes/BadgedTabItemView.swift
@@ -91,7 +91,7 @@ open class BadgedTabItemView: TabItemView {
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            badgeView.centerYAnchor.constraint(equalTo: topAnchor, constant: 0.0),
+            badgeView.topAnchor.constraint(equalTo: topAnchor, constant: -2.0),
             badgeView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0.0),
             badgeView.widthAnchor.constraint(greaterThanOrEqualTo: badgeView.heightAnchor, multiplier: 1.0, constant: 0.0)
             ])
